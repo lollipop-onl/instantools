b7a669dac8f0fe3fc49ba484a6098667db235d8b
simochee <lollipop@simochee.net>
2023-08-16 14:00:19 +0900

    ⚙️ setup TailwindCSS

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,ts,svelte}"],
+  theme: {
+    fontFamily: {
+      body: ["Zen Maru Gothic", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
